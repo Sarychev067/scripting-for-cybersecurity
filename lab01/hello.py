f372124370b66fcb5b568dca061f0b1b94d4a033
Sarychev067 <fortventer@gmail.com>
2026-09-14 14:34:20 +0000

    first commit

---
+++ lab01/hello.py
@@ -0,0 +1,8 @@
+name = input("What is your name? : ")
+course = input("What course are you studying? :")
+area  = input("What area of cybersecurity interests you? :")
+
+
+print(f"Hello, {name}")
+print(f"You are studying {course}")
+print(f"You are interested in {area}")
---
+++ lab01/student.py
@@ -0,0 +1,8 @@
+name = input("Enter yur name? : ")
+student_number = input("Enter student number :")
+year  = input("Enter your year of study :")
+
+
+print(f"Name:{name}")
+print(f"Student Number: {student_number}")
+print(f"Year: {year}")
